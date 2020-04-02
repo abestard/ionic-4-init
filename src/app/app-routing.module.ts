fe0c21a2dbd01834e2af0fc809b4b4abec436a36
bestard <jbestard123@gmail.com>
2020-04-02 13:42:26 -0400

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
-  { path: 'first-with-tabs', loadChildren: './pages/first-with-tabs/first-with-tabs.module#FirstWithTabsPageModule' },
-  { path: 'second', loadChildren: './pages/second/second.module#SecondPageModule' },
-  { path: 'tab1', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
-  { path: 'tab2', loadChildren: './pages/tab2/tab2.module#Tab2PageModule' },
-  { path: 'tab3', loadChildren: './pages/tab3/tab3.module#Tab3PageModule' },
-  { path: 'details', loadChildren: './pages/details/details.module#DetailsPageModule' },
-  { path: 'add-user', loadChildren: './pages/add-user/add-user.module#AddUserPageModule' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
